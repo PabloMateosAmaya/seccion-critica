8688c6b84f93c3702db957b8b32a513c509d8347
alumno <pabmat02@ucm.es>
2023-02-09 10:27:07 +0100

    yo ya no se nada

--- 01.py
+++ 01.py
@@ -4,7 +4,7 @@ from multiprocessing import Value, Array
 N = 8
 def task(common, tid, turn):
     a = 0
-    for i in range(100):
+    for i in range(20):
         print(f'{tid}−{i}: Non−critical Section')
         a += 1
         print(f'{tid}−{i}: End of non−critical Section')
